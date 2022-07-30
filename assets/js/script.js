ce8a18cc8a894bb09015112798b4c38a3c9d3088
Ali Samir <alisamir.2020@azhar.edu.eg>
2022-07-30 16:53:39 +0200

    Nav Menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,30 +8,17 @@ const overlay = document.querySelector("[data-overlay]");
 const elemArr = [navCloseBtn, overlay, navOpenBtn];
 
 for (let i = 0; i < elemArr.length; i++) {
-    elemArr[i].addEventListener("click", function() {
-        navbar.classList.toggle("active");
-        overlay.classList.toggle("active");
-    });
+  elemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
 }
 
 const navbarLinks = document.querySelectorAll("[data-navbar-link]");
 
 for (let i = 0; i < navbarLinks.length; i++) {
-    navbarLinks[i].addEventListener("click", function() {
-        navbar.classList.toggle("active");
-        overlay.classList.toggle("active");
-    });
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
 }
-
-const header = document.querySelector("[data-header]");
-const goTopBtn = document.querySelector("[data-go-top]");
-
-window.addEventListener("scroll", function() {
-    if (window.scrollY >= 400) {
-        header.classList.add("active");
-        goTopBtn.classList.add("active");
-    } else {
-        header.classList.remove("active");
-        goTopBtn.classList.remove("active");
-    }
-});
